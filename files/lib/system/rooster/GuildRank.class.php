605ff5ea28caadabdb91360b44714d14458b20a2
Rene Gerritsen <rene.gerritsen@icloud.com>
2015-10-14 10:11:26 +0200

    Fallback for Custom Rank Background Image

--- files/lib/system/rooster/GuildRank.class.php
+++ files/lib/system/rooster/GuildRank.class.php
@@ -76,13 +76,10 @@ class GuildRank extends RoosterCollection
      */
     public function getImage()
     {
-        if (!$this->isIsText()) {
-            return
-                $this->baseUrl .
-                '/class/thumbnail/guildrank/' .
-                $this->name .
-                '.png';
+        if (!$this->isIsText() && $this->name !== '') {
+            return $this->name;
         }
+        return 'data:image/gif;base64,R0lGOD lhCwAOAMQfAP////7+/vj4+Hh4eHd3d/v7+/Dw8HV1dfLy8ubm5vX19e3t7fr 6+nl5edra2nZ2dnx8fMHBwYODg/b29np6eujo6JGRkeHh4eTk5LCwsN3d3dfX 13Jycp2dnevr6////yH5BAEAAB8ALAAAAAALAA4AAAVq4NFw1DNAX/o9imAsB tKpxKRd1+YEWUoIiUoiEWEAApIDMLGoRCyWiKThenkwDgeGMiggDLEXQkDoTh CKNLpQDgjeAsY7MHgECgx8YR8oHwNHfwADBACGh4EDA4iGAYAEBAcQIg0Dk gcEIQA7';
     }
 
     /**
